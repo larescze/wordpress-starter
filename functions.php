43c4bea2da74637af3185382f0bfb8a5254d21be
Willi Lazarov <lazarov.w@gmail.com>
2021-12-03 16:37:08 +0100

    Update functions

--- functions.php
+++ functions.php
@@ -6,3 +6,36 @@ require_once locate_template('/functions/widgets.php');
 
 add_theme_support('post-thumbnails');
 add_theme_support('title-tag');
+
+// Exclude WordPress administration
+if (!is_admin()) {
+    // Make sure the path is correct
+    wp_enqueue_style(
+        'main-style',
+        get_template_directory_uri() . '/css/style.min.css',
+        array(),
+        null,
+        'all'
+    );
+
+    // Make sure the path is correct
+    wp_enqueue_script(
+        'main-js',
+        get_template_directory_uri() . '/js/main.js',
+        array(),
+        null,
+        true
+    );
+
+    function theme_enqueue_style()
+    {
+        wp_enqueue_style('main-style', get_stylesheet_uri());
+    }
+
+    function theme_enqueue_scripts()
+    {
+        wp_enqueue_style('main-js', get_stylesheet_uri());
+    }
+
+    add_action('wp_enqueue_scripts', 'theme_enqueue_style', 'theme_enqueue_scripts');
+}
